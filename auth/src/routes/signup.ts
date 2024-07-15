001ce40841c0ac88989535feb8cfedcb3c30f2e0
Roger Rigoni <rogerrigoni.a@gmail.com>
2024-07-15 11:36:00 -0300

    feat add jwt to auth

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,5 +1,7 @@
 import express, { Request, Response } from 'express';
 import { body, validationResult } from 'express-validator';
+import jwt from 'jsonwebtoken';
+
 import { User } from '../models/user';
 import { RequestValidationError } from '../errors/request-validaton-error';
 import { BadRequestError } from '../errors/bad-request-error';
@@ -31,6 +33,21 @@ router.post(
 
     const user = User.build({ email, password });
     await user.save();
+
+    // Generate JWT
+    const userJwt = jwt.sign(
+      {
+        id: user.id,
+        email: user.email,
+      },
+      'key'
+    );
+
+    //Store it on session object
+    req.session = {
+      ...req.session,
+      jwt: userJwt,
+    };
 
     return res.status(201).send(user);
   }
