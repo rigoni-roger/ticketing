44ff2a2e7aa5a8714c54e674ab6626b491e69201
Roger Rigoni <rogerrigoni.a@gmail.com>
2024-07-16 21:48:07 -0300

    feat: implement auth

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,9 +1,9 @@
 import express, { Request, Response } from 'express';
-import { body, validationResult } from 'express-validator';
+import { body } from 'express-validator';
 import jwt from 'jsonwebtoken';
 
+import { validateRequest } from '../middlwares/validate-request';
 import { User } from '../models/user';
-import { RequestValidationError } from '../errors/request-validaton-error';
 import { BadRequestError } from '../errors/bad-request-error';
 
 const router = express.Router();
@@ -17,13 +17,8 @@ router.post(
       .withMessage('Password must be between 4 and 20 characters'),
     body('email').isEmail().withMessage('Email must be valid'),
   ],
+  validateRequest,
   async (req: Request, res: Response) => {
-    const errors = validationResult(req);
-
-    if (!errors.isEmpty()) {
-      throw new RequestValidationError(errors.array());
-    }
-
     const { email, password } = req.body;
     const existingUser = await User.findOne({ email });
 
