4be018a5878dfb9d5507983b4e45aaff7b180293
Roger Rigoni <rogerrigoni.a@gmail.com>
2024-07-21 09:36:56 -0300

    feat: add show and new api

---
+++ tickets/src/app.ts
@@ -0,0 +1,36 @@
+import express from 'express';
+import 'express-async-errors';
+import { json } from 'body-parser';
+import cookieSession from 'cookie-session';
+
+import { createTicketRouter } from './routes/new';
+
+import {
+  errorHandler,
+  NotFoundError,
+  currentUser,
+} from '@rigonitickets/common';
+import { showTicketRouter } from './routes/show';
+
+const app = express();
+app.set('trust proxy', true);
+app.use(json());
+// using cookies to store session data
+app.use(
+  cookieSession({
+    signed: false,
+    secure: process.env.NODE_ENV !== 'test',
+  })
+);
+app.use(currentUser);
+
+app.use(showTicketRouter);
+app.use(createTicketRouter);
+
+app.all('*', async () => {
+  throw new NotFoundError();
+});
+
+app.use(errorHandler);
+
+export { app };
---
+++ tickets/src/test/setup.ts
@@ -0,0 +1,49 @@
+import { MongoMemoryServer } from 'mongodb-memory-server';
+import mongoose from 'mongoose';
+import jwt from 'jsonwebtoken';
+
+declare global {
+  var signin: () => string[];
+}
+
+let mongo: any;
+
+jest.setTimeout(120000); // Increase the timeout to 2 minutes
+beforeAll(async () => {
+  process.env.JWT_KEY = 'key';
+
+  mongo = await MongoMemoryServer.create();
+  const mongoUri = mongo.getUri();
+
+  await mongoose.connect(mongoUri, {});
+});
+
+beforeEach(async () => {
+  const collections = await mongoose.connection.db.collections();
+
+  for (let collection of collections) {
+    await collection.deleteMany({});
+  }
+});
+
+afterAll(async () => {
+  await mongo.stop();
+  await mongoose.connection.close();
+});
+
+global.signin = () => {
+  const payload = {
+    email: 'test@test.com',
+    id: '1lk24h5h65j4',
+  };
+
+  const token = jwt.sign(payload, process.env.JWT_KEY!);
+
+  const session = { jwt: token };
+
+  const sessionJSON = JSON.stringify(session);
+
+  const base64 = Buffer.from(sessionJSON).toString('base64');
+
+  return [`session=${base64}`];
+};
